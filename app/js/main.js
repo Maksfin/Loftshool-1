dbb22eddb7b7ccedcfdd9e1bb88e63f188e38b9d
Maks Roschin <dyrnoj@bk.ru>
2015-10-04 16:32:26 +0300

    finished my_works

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
-jQuery(document).ready(function($) {
-	console.log('Это главная страница');
+$(document).ready(function($) {
+	$('.galery__add-image').on('click', function(e){
+		e.preventDefault();
+
+		$('.popup').bPopup({
+			closeClass : 'popup__close',
+			easing: 'easeOutBack', //использую плагин jQuery easing plugin
+      speed: 550,
+      transition: 'slideDown'
+		});
 });
+
+$('.form__input-file-origin').on('change', function(){
+	var 
+		$this = $(this),
+		value = $this.val(),
+		pureVal = value.replace(/c:\\fakepath\\/gmi, "");
+
+	$('.form__input-fake').text(pureVal);
+	});	
+// $('.form__input-file-hidden').on('change', function(){
+// 		var 
+// 			input = $(this),
+// 			name = input[0].files[0].name;
+// 			$('.input-file-text')
+// 					.val(name)
+// 					.trigger('hideTooltip')
+// 					.removeClass('has-error');
+// });
+
+}); // end;
